3845314782ee5fb11e2062b187f770d0164a1c7d
Nilayan Paul <nlpl931@gmail.com>
2018-05-11 15:56:13 +0530

    List files from local directory!
    Signed-off-by: Nilayan Paul <nlpl931@gmail.com>

--- app/src/main/java/nlpl/com/a3dm/print_fragment.java
+++ app/src/main/java/nlpl/com/a3dm/print_fragment.java
@@ -1,6 +1,7 @@
 package nlpl.com.a3dm;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -8,30 +9,45 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.ArrayList;
 
 /* TODO add file R/W support
     add file access permissions
  */
 public class print_fragment extends Fragment {
 
+    Spinner sp;
     String dir = null;
+    ArrayList<String> list = new ArrayList<String>();
+    ArrayAdapter<String> arr;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        dir = Environment.getExternalStorageDirectory().toString();
+        dir = Environment.getExternalStorageDirectory().toString() + getResources().getString(R.string.local_path);
+        File localStorage = new File(dir);
+        File[] files = localStorage.listFiles();
+        if(files.length>0){
+            for(int i=0; i<files.length; i++)
+                list.add(files[i].getName());
+        }
     }
-//TODO get proper R/W code and custom file associations.
+
+    //TODO get proper R/W code and custom file associations.
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View rootview = inflater.inflate(R.layout.print_fragment, container, false);
-        TextView tv = rootview.findViewById(R.id.printinfo);
-        tv.setText(dir);
+        sp = rootview.findViewById(R.id.spinner2);
+        arr = new ArrayAdapter<String>(rootview.getContext(), android.R.layout.simple_spinner_dropdown_item, list);
+        sp.setAdapter(arr);
         return rootview;
     }
 
