745e54c732ded60f18096abbfa12e6595ae04d7f
Nilayan Paul <nlpl931@gmail.com>
2018-05-11 14:10:46 +0530

    App can now create its own local storage to access gcode!
    Signed-off-by: Nilayan Paul <nlpl931@gmail.com>

--- app/src/main/java/nlpl/com/a3dm/intro.java
+++ app/src/main/java/nlpl/com/a3dm/intro.java
@@ -5,12 +5,17 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.io.File;
 
 
 /**
@@ -34,9 +39,20 @@ public class intro extends Fragment {
         {
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, getResources().getInteger(R.integer.STORAGE_WRITE_PERM));
         }
+        chkLocalStorage(rv);
         return rv;
+
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+    // TODO: Rename method, add check for local storage
+    public void chkLocalStorage(View v)
+    {
+        String path_to_dir = Environment.getExternalStorageDirectory().toString() + getResources().getString(R.string.local_path);
+        File localDir = new File(path_to_dir);
+        if (!localDir.exists())
+        {
+            localDir.mkdir();
+        }
+    }
 
 }
