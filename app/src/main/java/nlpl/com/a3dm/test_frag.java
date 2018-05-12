dbd5b6c35c5488502f1239d2af520434bf984707
Nilayan Paul <nlpl931@gmail.com>
2018-05-12 20:19:19 +0530

    UI improvements.
    Signed-off-by: Nilayan Paul <nlpl931@gmail.com>

--- app/src/main/java/nlpl/com/a3dm/test_frag.java
+++ app/src/main/java/nlpl/com/a3dm/test_frag.java
@@ -7,9 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class test_frag extends Fragment {
+
+    Spinner temperature;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,7 +25,11 @@ public class test_frag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_test, container, false);
+        temperature = rootView.findViewById(R.id.temp_spinner);
+        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<Integer>(rootView.getContext(), android.R.layout.simple_spinner_dropdown_item, R.id.temp_spinner);
+        temperature.setAdapter(arrayAdapter);
+        return rootView;
     }
 
 }
