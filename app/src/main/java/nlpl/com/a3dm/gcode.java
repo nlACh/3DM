0b63351ed650ef4c46be82dc4a5ef08218ec4d0e
Nilayan Paul <nlpl931@gmail.com>
2018-05-15 20:56:17 +0530

    Dummy GCODE support added.
    Signed-off-by: Nilayan Paul <nlpl931@gmail.com>

---
+++ app/src/main/java/nlpl/com/a3dm/gcode.java
@@ -0,0 +1,16 @@
+package nlpl.com.a3dm;
+
+public class gcode {
+
+    //Only linear moves used G1 and homing G28
+    public void g_(int x)
+    {
+        //TODO: add new functions for the GCODE
+    }
+
+    //for commands like M119 M84.
+    public void m_(int x)
+    {
+        //TODO: add new functions for the GCODE
+    }
+}
--- app/src/main/java/nlpl/com/a3dm/test_frag.java
+++ app/src/main/java/nlpl/com/a3dm/test_frag.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -19,12 +20,15 @@ import android.widget.Toast;
 
 public class test_frag extends Fragment {
 
+    gcode Gcode = new gcode();
     Spinner temp_spinn;
     int temperature_val = 0;
 
     //Heater switch
     Switch sw;
 
+    //now the buttons.
+    ImageButton xm, xp, ym, yp;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +40,20 @@ public class test_frag extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_test, container, false);
+
+        //set handlers for the buttons.
+        xm = rootView.findViewById(R.id.x_minus);
+        xp = rootView.findViewById(R.id.x_plus);
+        ym = rootView.findViewById(R.id.y_minus);
+        yp = rootView.findViewById(R.id.y_plus);
+
+        //spinner.
         temp_spinn = rootView.findViewById(R.id.temp_spinner);
+
+        //Switch.
         sw = rootView.findViewById(R.id.switch1);
+
+        //add a listener for the spinner
         sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -47,6 +63,7 @@ public class test_frag extends Fragment {
                     Toast.makeText(rootView.getContext(), "Set heater off!" + Integer.toString(temperature_val), Toast.LENGTH_SHORT).show();
             }
         });
+        //add a listener for the switch
         temp_spinn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -61,12 +78,15 @@ public class test_frag extends Fragment {
                 temperature_val = 0;
             }
         });
+
+        //TODO: add event handler for the buttons.
         return rootView;
     }
 
     public void setTemp(int x, View v)
     {
         Toast.makeText(v.getContext(), String.valueOf(x), Toast.LENGTH_SHORT).show();
+        //TODO add temperature control.
     }
 
 }
